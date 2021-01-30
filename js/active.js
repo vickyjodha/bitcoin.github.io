014620266da96a06e44a40ff550a1511436742d6
vikram700 <vs4110690@gmail.com>
2021-01-30 22:40:20 +0530

    gjhfd

--- js/active.js
+++ js/active.js
@@ -52,3 +52,40 @@
     });
 
 })(jQuery);
+
+ // year 
+      var date = new Date().getFullYear();
+$(".year").append(date);
+    
+
+var $owl = $('#carousel1');
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index);
+
+});
+$('#carousel1').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+
+    responsive: {
+        0: {
+            items: 1
+        },
+        680: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+$(document).on('click', '.owl-item>div', function () {
+    var $speed = 1000;
+    $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
+});
